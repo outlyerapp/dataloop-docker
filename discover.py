da9acf36c3058e1e786a7d10292389ed25aba512
Tom Ashley <tom.ashley@dataloop.io>
2015-10-02 13:39:52 +0100

    make discover more resilient to errors

--- discover.py
+++ discover.py
@@ -25,7 +25,11 @@ def create_finger():
 
 
 def agent_name_to_finger(name):
-    _resp = requests.get(API + "/api/agents", headers=api_header()).json()
+    try:
+        _resp = requests.get(API + "/api/agents", headers=api_header()).json()
+    except Exception as E:
+        print "Failed to return agent details: %s" % E
+        return "" # send an empty finger if we don't get one back
     for _agent in _resp:
         if _agent['name'] == name:
             return _agent['id']
@@ -33,18 +37,25 @@ def agent_name_to_finger(name):
 
 def get_agents():
     # only get agents from dataloop that match the mac address of dl-dac container
-    _resp = requests.get(API + "/api/agents", headers=api_header())
+    try:
+        _resp = requests.get(API + "/api/agents", headers=api_header())
+    except Exception as E:
+        print "Failed to query agents: %s" % E
+        return False
+
+    # print "get_agents response: %s " % _resp.text
+
     agents = []
     if _resp.status_code == 200:
         for l in _resp.json():
             if l['mac'] == get_mac():
                 agents.append(l['name'])
-        if len(agents) == 0:
-            return []
-        else:
-            return agents
+
+        return agents
+
     else:
-        return []
+        print "Bad response returning agent list: %s" % _resp.status_code
+        return False
 
 
 def get_agents_details(finger):
@@ -84,45 +95,58 @@ def create_agent(container):
     resp = register_agent(_finger, data)
     if resp.status_code == 200:
         pass
-        # print "successfully registered %s" % _finger
+        print "successfully registered %s" % _finger
     else:
         print "registration of %s failed with status code %d" % (_finger, resp.status_code)
 
 
 def deregister_agent(finger):
-    return requests.post(EXCHANGE + '/agents/' + finger + '/deregister', headers=api_header())
+    try:
+        requests.post(EXCHANGE + '/agents/' + finger + '/deregister', headers=api_header())
+    except Exception as E:
+        print "Failed to deregister agent: %s" % E
+
+    print "successfully deleted agent: %s" % finger
+    return True
 
 
 def get_containers():
     _containers = []
-    _resp = requests.get(CADVISOR + '/api/v1.3/docker').json()
+    try:
+        _resp = requests.get(CADVISOR + '/api/v1.3/docker').json()
+    except Exception as E:
+        print "Failed to query containers: %s" % E
+        return False
+
     for k, v in _resp.iteritems():
-        _containers.append(k.replace('/docker/', '')[:12])
+        ## TODO: v['stats'] contains all the metrics for this container
+        _containers.append(v['name'].replace('/docker/', '')[:12])
     return _containers
 
 
 print "Container Auto-Discovery running. Press ctrl+c to exit!"
 while True:
-    
+
     agents = get_agents()
-    # print "dataloop agents: %s" % agents
+    # print "dataloop agents: %s" % len(agents)
 
     containers = get_containers()
-    # print "cadvisor containers: %s" % containers
+    # print "cadvisor containers: %s" % len(containers)
 
     # add agents that don't exist in Dataloop
-    for container in containers:
-        if container not in agents:
-            print "adding container: %s" % container
-            create_agent(container)
+    if len(containers)>=0 and len(agents)>=0:
+        for container in containers:
+            if container not in agents:
+                print "adding container: %s" % container
+                create_agent(container)
 
 
-    # delete agents that don't exist as containers
-    for agent in agents:
-        if agent not in containers:
-            print "deleting agent: %s" % agent
-            finger = agent_name_to_finger(agent)
-            deregister_agent(finger)
+        # delete agents that don't exist as containers
+        for agent in agents:
+            if agent not in containers:
+                print "deleting agent: %s" % agent
+                finger = agent_name_to_finger(agent)
+                deregister_agent(finger)
 
     sleep(5)  # have a little rest
 
