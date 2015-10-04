f2bd4c32b7de2cc75aeaebb0c0810fecad4dcb23
sacreman <sacreman@gmail.com>
2015-10-05 00:22:02 +0100

    first working metric sender

--- metrics.py
+++ metrics.py
@@ -2,11 +2,13 @@
 import requests
 import uuid
 from time import sleep
-import sys
+import socket
 
 API_KEY = ''
 API = 'https://www.dataloop.io'
 CADVISOR = 'http://192.168.99.100:8080'  # CAdvisor container URL. Change this if on Linux.
+GRAPHITE_SERVER = 'graphite.dataloop.io'
+GRAPHITE_PORT = 2003
 
 
 def api_header():
@@ -16,10 +18,10 @@ def api_header():
 def get_mac():
     return str(uuid.getnode())
 
+
 def flatten(structure, key="", path="", flattened=None):
     if flattened is None:
         flattened = {}
-
     if type(structure) not in (dict, list):
         flattened[((path + ".") if path else "") + key] = structure
     elif isinstance(structure, list):
@@ -37,7 +39,6 @@ def get_agents():
     agents = {}
     if _resp.status_code == 200:
         for l in _resp.json():
-            print l['id']
             if l['mac'] == get_mac():
                 name = l['name']
                 agents[name] = l['id']
@@ -54,8 +55,6 @@ def get_metrics():
         return False
     
     for k, v in _resp.iteritems():
-	# print v['stats']
-        ## TODO: v['stats'] contains all the metrics for this container
         _containers.append(v['name'].replace('/docker/', '')[:12])
         name = v['name'].replace('/docker/', '')[:12]
         _metrics[name] = v['stats']
@@ -63,73 +62,35 @@ def get_metrics():
 
 
 # send metrics to graphite
-def send_msg(message, graphite_host, graphite_port):
-    print "Sending message:\n%s" % message
-    return # For testing
-
+def send_msg(message):
+    # print "Sending message:\n%s" % message
     try:
         sock = socket.socket()
-    except socket.error, msg:
-        print 'CRITICAL - Failed to create socket. Error code: %s - %s' % (msg[0], msg[1])
-        sys.exit(2)
-    try:
-        sock.connect((graphite_host, int(graphite_port)))
+        sock.connect((GRAPHITE_SERVER, GRAPHITE_PORT))
+        sock.sendall(message)
+        sock.close()
     except Exception, e:
-        print('CRITICAL - something is wrong with %s:%s. Exception is %s' % (graphite_host, graphite_port, e))
-        sys.exit(2)
-    sock.sendall(message)
-    sock.close
+        print('CRITICAL - something is wrong with %s:%s. Exception is %s' % (GRAPHITE_SERVER, GRAPHITE_PORT, e))
 
 
-    # metrics = []
-
-    # for timestamp in times:
-    #     index =  times.index(timestamp)
-    #     # TODO some clever rollup for zeros
-    #     metrics.append(("%s.%s %s%s %s") % (FINGER, PATH, values[index], UOM, int(timestamp) / 1000))
-
-    # message =  '\n'.join(metrics)
-    # send_msg(message, GRAPHITE_SERVER, PORT)
-
-
-agents = get_agents()
-
-metrics =  get_metrics()
-# print metrics.keys()
-
-flat_metrics = {}
-for container,v in metrics.iteritems():
-    # print container
-    finger = agents[container]
-    flat_metrics[container] = {}
-    for a in v:
-        # print a.keys()
-        # print a['timestamp']
-        for m in ['network', 'diskio', 'memory', 'cpu']:
-            z = flatten(a[m], key=m, path=finger)
-            flat_metrics[container].update(z)
-            
-
-for c,d in flat_metrics.iteritems():
-    # print c
-    for path, value in d.iteritems():
-       print "%s %s" % ( path, value )
-       # pass
-		
-
-sys.exit(0)
-
+print "Container Metric Send running. Press ctrl+c to exit!"
 while True:
-    # For each agent in Dataloop pull out the fingerprint
+    agents = get_agents()
+    metrics = get_metrics()
 
+    flat_metrics = {}
+    for container, v in metrics.iteritems():
+        finger = agents[container]
+        flat_metrics[container] = {}
+        for a in v:
+            for m in ['network', 'diskio', 'memory', 'cpu']:
+                z = flatten(a[m], key=m, path=finger)
+                flat_metrics[container].update(z)
 
-    # Get the metrics from CAdvisor
-
-
-    # Prefix metrics with correct fingerprint
-
-
-    # Send to Graphite port!
-
+    for c, d in flat_metrics.iteritems():
+        for path, value in d.iteritems():
+            if isinstance(value, int):
+                message = "%s %s\n" % (path, value)
+                send_msg(message)
 
     sleep(5)    #  sleepy time
