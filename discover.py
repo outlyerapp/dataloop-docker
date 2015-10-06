51509340112b4394b233c36440ae070bcf262b16
Tom Ashley <tom.ashley@dataloop.io>
2015-10-06 11:01:49 +0100

    [discover] only register new containers, delete missing

--- discover.py
+++ discover.py
@@ -178,7 +178,7 @@ def main(argv):
         # print "cadvisor containers: %s" % len(containers)
 
         # add agents that don't exist in Dataloop
-        if len(containers)>=0 and len(agents)>=0:
+        if len(containers)>0:
             for container in containers:
                 if container not in agents:
                     print "adding container: %s" % container
@@ -191,8 +191,8 @@ def main(argv):
                     ping(finger, container)
 
 
-
-            # delete agents that don't exist as containers
+        # delete agents that don't exist as containers
+        if len(agents)>0:
             for agent in agents:
                 if agent not in containers:
                     #print "deleting agent: %s" % agent
