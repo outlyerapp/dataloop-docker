270b3b88f3abe49260b7d607d3356b8889d776ad
Tom Ashley <tom.ashley@dataloop.io>
2015-10-22 09:58:16 +0100

    take the leap and have one discover

--- discover.py
+++ discover.py
@@ -1,133 +1,158 @@
-#!/usr/bin/env python
-import sys
-import os
 import getopt
-import requests
+import logging
+import os
+import socket
+import threading
+import sys
+import time
 import uuid
-import json
 import grequests
-from time import sleep
-from socket import gethostname
+import requests
 from docker.client import Client
 from docker.utils import kwargs_from_env
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    stream=sys.stdout,
+                    level=logging.DEBUG)
 
-API_KEY = ''  # You need to set this!
-EXCHANGE_URL = 'https://agent.dataloop.io'
-API_URL = 'https://www.dataloop.io'
-CADVISOR = 'http://127.0.0.1:8080'
-
-# Don't touch anything below this point. In fact don't even scroll down.
+register = []
+uuid_hash = uuid.UUID('12345678123456781234567812345678')
 
 if os.path.exists('/rootfs/var/run/docker.sock'):
     docker_cli = Client(base_url='unix://rootfs/var/run/docker.sock', version='auto')
 else:
     docker_cli = Client(**kwargs_from_env(assert_hostname=False))
 
-
-def api_header():
-    return {"Content-type": "application/json", "Authorization": "Bearer " + API_KEY}
-
-
-def get_mac():
-    return requests.get(CADVISOR + '/api/v1.3/machine').json()['system_uuid']
-
-
-def create_finger():
-    return str(uuid.uuid4())
-
-
-def agent_name_to_finger():
-    agent_fingers = {}
-    _resp = requests.get(API_URL + "/api/agents", headers=api_header()).json()
-    for _agent in _resp:
-        agent_fingers[_agent['name']] = _agent['id']
-    return agent_fingers
-
-
-def get_agents():
-    agents = []
-    _resp = requests.get(API_URL + "/api/agents", headers=api_header())
-    if _resp.status_code == 200:
-        for l in _resp.json():
-            if l['mac'] == get_mac():
-                agents.append(l['name'])
-        return list(set(agents))
-
-
-def get_agents_details(finger):
-    _resp = requests.get(API_URL + "/api/agents/" + finger, headers=api_header())
-    if _resp.status_code == 200:
-        return json.loads(_resp.text)
-    else:
-        print "failed to get agent details"
-
-
-def register_agent(finger, data):
-    return requests.post(EXCHANGE_URL + '/agents/' + finger + '/register', json=data, headers=api_header())
-
-
-def create_agent(container):
-    _finger = create_finger()
-    data = {
-        'fingerprint': _finger,
-        'tags': '',
-        'name': container,
-        'hostname': gethostname(),
-        'mac': get_mac(),
-        'os_name': 'docker',
-        'os_version': '',
-        'container': '',
-        'processes': get_processes(container),
-        'interfaces': get_network(container),
-        'mode': 'solo',
-        'version': '',
-        'interpreter': ''
+def get_request_headers(ctx):
+    return {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + ctx['api_key']
     }
 
-    resp = register_agent(_finger, data)
-    if resp.status_code == 200:
-        print "successfully registered %s" % _finger
-    else:
-        print "registration of %s failed with status code %d" % (_finger, resp.status_code)
+
+def get_host_mac(ctx):
+    # #TEST code
+    # return "macmacmacmac"
+    cadvisor_url = ctx['cadvisor_host'] + '/api/v1.3/machine'
+    return requests.get(cadvisor_url).json()['system_uuid']
 
 
-def de_register_agent(finger):
-    requests.post(EXCHANGE_URL + '/agents/' + finger + '/deregister', headers=api_header())
-    print "successfully deleted agent: %s" % finger
+def registration(ctx):
+    while True:
+        try:
+            register_sync(ctx)
+        except Exception as ex:
+            logger.error("register sync failed: %s" % ex, exc_info=True)
+        finally:
+            time.sleep(ctx['register_interval'])
 
 
-def ping(container_list, agent_fingers):
-    rs = []
-    for container in container_list:
-        data = {
-            'mac': get_mac(),
-            'hostname': str(gethostname()),
+def register_sync(ctx):
+    logger.info("register sync")
+
+    agents = get_agents(ctx)
+    agent_ids = get_agents_ids(agents)
+
+    containers = get_containers(ctx)
+    container_ids = get_container_ids(containers)
+    logger.debug("containers: %s", container_ids)
+
+    dead_containers = agent_ids - container_ids
+
+    destroy_agents(ctx, dead_containers)
+
+
+def get_containers(ctx):
+    cadvisor_url = ctx['cadvisor_host'] + "/api/v1.3/docker"
+    resp = requests.get(cadvisor_url).json()
+
+    # # TEST containers
+    # with open('../cadvisor.json') as conts:
+    #     resp = json.load(conts)
+
+    return resp.values()
+
+def get_container_ids(containers):
+    def get_id(c):
+        if 'system.slice' in c['name']:
+            return c['name'].replace('/system.slice/docker-', '')[:12]
+        else:
+            return c['name'].replace('/docker/', '')[:12]
+    return set(map(get_id, containers))
+
+
+def get_agents(ctx):
+    agent_api = ctx['api_host'] + "/api/agents"
+
+    resp = requests.get(agent_api, headers=get_request_headers(ctx))
+    resp.raise_for_status()
+    agents = resp.json()
+
+    host_mac = get_host_mac(ctx)
+
+    def filter_host(agent):
+        return agent['mac'] == host_mac
+
+    return filter(filter_host, agents)
+
+
+def get_agents_ids(agents):
+    def agent_name(agent):
+        return agent['name']
+
+    return set(map(agent_name, agents))
+
+def destroy_agents(ctx, agent_ids):
+    api_host = ctx['api_host']
+    headers = get_request_headers(ctx)
+
+    def create_request(id):
+        finger = uuid.uuid5(uuid_hash, id)
+        url = "%s/api/agents/%s/deregister" % (api_host, finger,)
+        return grequests.post(url, headers=headers)
+
+    reqs = map(create_request, agent_ids)
+    grequests.map(reqs)
+
+
+def ping(ctx):
+    logger.debug("pinging")
+    try:
+        containers = get_containers(ctx)
+        container_ids = get_container_ids(containers)
+        ping_containers(ctx, container_ids)
+    except Exception as ex:
+        logger.error("register sync failed: %s" % ex, exc_info=True)
+
+
+def ping_containers(ctx, container_ids):
+    api_host = ctx['api_host']
+    headers = get_request_headers(ctx)
+    host_mac = get_host_mac(ctx)
+    host_name = str(socket.gethostname())
+
+    def create_request(id):
+        details = {
+            'mac': host_mac,
+            'hostname': host_name,
             'tags': '',
             'os_name': 'docker',
             'os_version': '',
             'container_name': '',
-            'proc_list': get_processes(container),
+            'proc_list': get_processes(id),
             'ip': '',
-            'interfaces': get_network(container),
+            'interfaces': get_network(id),
             'mode': 'solo',
-            'name': str(container)
+            'name': str(id)
         }
-        finger = agent_fingers[container]
-        url = API_URL + '/api/agents/' + finger + '/ping'
-        rs.append(grequests.post(url, json=data, headers=api_header()))
-    return grequests.map(rs)
+        finger = uuid.uuid5(uuid_hash, id)
+        url = "%s/api/agents/%s/ping" % (api_host, finger,)
+        return grequests.post(url, json=details, headers=headers)
 
-
-def get_containers():
-    _containers = []
-    _resp = requests.get(CADVISOR + '/api/v1.3/docker').json()
-    for k, v in _resp.iteritems():
-        if 'system.slice' in v['name']:
-            _containers.append(v['name'].replace('/system.slice/docker-', '')[:12])
-        else:
-            _containers.append(v['name'].replace('/docker/', '')[:12])
-    return _containers
+    reqs = map(create_request, container_ids)
+    grequests.map(reqs)
 
 
 def get_processes(container):
@@ -167,65 +192,40 @@ def get_network(container):
     return interface
 
 
-def sync():
-    try:
-        agents = get_agents()
-        containers = get_containers()
-        agent_fingers = agent_name_to_finger()
-
-        # add agents that don't exist
-        for container in containers:
-            if container not in agents:
-                print "adding container: %s" % container
-                create_agent(container)
-
-        # ping running containers
-        ping(containers, agent_fingers)
-
-        # delete agents that don't exist as containers
-        for agent in agents:
-            if agent not in containers:
-                finger = agent_fingers[agent]
-                de_register_agent(finger)
-
-    except Exception as E:
-        print "unable to sync containers!: %s" % E
-
-
 def main(argv):
-    global API_KEY, CADVISOR, API_URL, EXCHANGE_URL
+    ctx = {
+        "register_interval": 10,
+        "ping_interval": 10,
+        "api_host": "https://www.dataloop.io",
+        "cadvisor_host": "http://127.0.0.1:8080"
+    }
 
     try:
-        opts, args = getopt.getopt(argv, "ha:c:u:e::", ["apikey=", "cadvisor=", "apiurl=", "exchangeurl="])
+        opts, args = getopt.getopt(argv, "ha:c:u::", ["apikey=", "cadvisor=", "apiurl="])
     except getopt.GetoptError:
-        print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop api address:port> -e <dataloop exchange address:port>'
+        print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop api address:port>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop address:port> -e <dataloop exchange address:port>'
+            print 'discover.py -a <apikey> -c <cadvisor address:port> -u <dataloop address:port>'
             sys.exit()
         elif opt in ("-a", "--apikey"):
-            API_KEY = arg
+            ctx['api_key'] = arg
         elif opt in ("-c", "--cadvisor"):
-            CADVISOR = arg
+            ctx['cadvisor_host'] = arg
         elif opt in ("-u", "--apiurl"):
-            API_URL = arg
-        elif opt in ("-e", "--exchangeurl"):
-            EXCHANGE_URL = arg
+            ctx['api_host'] = arg
 
-    print 'apikey: ' + API_KEY
-    print 'api url: ' + API_URL
-    print 'exchange url: ' + EXCHANGE_URL
-    print 'cadvisor endpoint: ' + CADVISOR
-    print 'initial containers: ' + str(get_containers())
-    print 'initial agents: ' + str(get_agents())
+    reg = threading.Thread(
+        name="register",
+        target=registration,
+        args=(ctx,)
+    )
+    reg.start()
 
-    print "Container Auto-Discovery running. Press ctrl+c to exit!"
     while True:
-
-        sync()
-        sleep(5)
-
+        ping(ctx)
+        time.sleep(ctx['ping_interval'])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
