06e461fd72085748d38feaa6af4cbe11d7661677
sacreman <sacreman@gmail.com>
2016-10-13 11:38:27 +0100

    fix server address

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ from prometheus_client.parser import text_string_to_metric_families
 bucket = 'c67767a3-709e-5970-89fe-ad090e333687'
 
 prom_data = requests.get('http://localhost:9100/metrics').content
-influxdb_url = 'http://influxdb.dataloop.io:8086/write?db=node_exporter'
+influxdb_url = 'http://influxdb.dataloop.io:8086/write?db=influxdb'
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 epoch = str(int(time.time()*1000000000))
 payload = ''
@@ -21,6 +21,7 @@ for family in text_string_to_metric_families(prom_data):
             for k, v in tags.iteritems():
                 tag_string += "," + k + "=" + v
         # payload_line = "cpu,host=server01,region=us-west value=0.64 1434055562000000000\n"
+        # payload_line = metric + tag_string + ' value=' + value + '\n'
         payload_line = metric + tag_string + ' value=' + value + " " + epoch + '\n'
         payload += payload_line
 
