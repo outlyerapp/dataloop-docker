38a66948f8a494808a4a0873f2c19adabe1fa790
sacreman <sacreman@gmail.com>
2016-10-15 19:28:26 +0100

    support coreos

--- dl_lib.py
+++ dl_lib.py
@@ -128,6 +128,8 @@ def get_container_id(path):
         return path.replace('/system.slice/docker-', '')[:12]
     elif 'system.slice/docker.service/docker/' in path:
         return path.replace('/system.slice/docker.service/docker/', '')[:12]
+    elif '/init.scope/system.slice/docker-' in path:
+        return path.replace('/init.scope/system.slice/docker-', '')[:12]
     else:
         return path.replace('/docker/', '')[:12]
 
