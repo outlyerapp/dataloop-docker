d1c3c1f8a8582b1fa0b14e2e0352c86d38655e8c
sacreman <sacreman@gmail.com>
2016-10-15 19:29:28 +0100

    clean up container id detection from path

--- dl_lib.py
+++ dl_lib.py
@@ -124,9 +124,9 @@ def get_container_labels(container):
 
 
 def get_container_id(path):
-    if 'system.slice/docker-' in path:
+    if '/system.slice/docker-' in path:
         return path.replace('/system.slice/docker-', '')[:12]
-    elif 'system.slice/docker.service/docker/' in path:
+    elif '/system.slice/docker.service/docker/' in path:
         return path.replace('/system.slice/docker.service/docker/', '')[:12]
     elif '/init.scope/system.slice/docker-' in path:
         return path.replace('/init.scope/system.slice/docker-', '')[:12]
